ade0c49217781e8d8014dbf445d3b287cc47231d
Bruno Silva <brunpersilva@gmail.com>
2021-04-15 01:03:37 -0300

    Added Toggle button on header

--- src/Components/MainHeader/index.tsx
+++ src/Components/MainHeader/index.tsx
@@ -1,6 +1,7 @@
 import React, {useMemo} from 'react';
 import emojis from '../../Utils/emojis'
 import{Container, Profile, Welcome, UserName} from './style'
+import Toggle from '../Toggle'
 
 const MainHeader: React.FC = () =>{
     const emoji = useMemo(()=> {
@@ -10,7 +11,8 @@ const MainHeader: React.FC = () =>{
 
     return(
         <Container>
-        <h1>Toogle</h1>
+            
+        <Toggle/>
 
         <Profile>
             <Welcome>Olá, {emoji}</Welcome>
---
+++ src/Components/Toggle/index.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+
+import{
+    Container,
+    ToggleLabel,
+    ToggleSelector,
+} from './style'
+
+const Toggle: React.FC = () => {    
+    const[online, setOnline] = useState(false);
+
+    return(
+    <Container>
+        <ToggleLabel>Light</ToggleLabel>
+
+        <ToggleSelector checked={online} uncheckedIcon={false} checkedIcon={false} onChange={() => setOnline(!online)}>
+
+        </ToggleSelector>
+        <ToggleLabel>Dark</ToggleLabel>
+    </Container>
+    )}
+export default Toggle;
